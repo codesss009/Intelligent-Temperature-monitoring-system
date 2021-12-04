5fe08b95a9463ad58d8dc2745120870c0c459897
codesss009 <72293035+codesss009@users.noreply.github.com>
2021-12-04 09:37:59 +0530

    Create visualise.js

---
+++ visualise.js
@@ -0,0 +1,8 @@
+setChartLibrary('google-chart');
+setChartTitle('Your Graph Title');
+setChartType('lineGraph');
+add(183);
+mul(0.0977);
+mul(1.8);
+setAxisName('time_stamp','temp');
+plotChart('time_stamp','temp');
